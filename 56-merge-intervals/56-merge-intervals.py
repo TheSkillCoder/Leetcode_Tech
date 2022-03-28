1c88145b08e68f5528ad33d55bc6388957b85e6c
TheSkillCoder <100764233+TheSkillCoder@users.noreply.github.com>
2022-03-28 06:41:08 +0530

    Time: 379 ms (5.09%), Space: 18.2 MB (35.53%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -14,13 +14,11 @@ class Solution:
 
             else:
                 x=intervals[j][0]
-                y=intervals[j][1]
                 while i>=0 and ans[i][1]>=intervals[j][0]:
                     x=min(ans[i][0], x)
-                    y=max(ans[i][1], y)
                     ans.pop()
                     i-=1
-                ans.append([x,y])
+                ans.append([x,intervals[j][1]])
                 i+=1
                 j+=1
                 
