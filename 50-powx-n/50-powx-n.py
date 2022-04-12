e1b16913280e70705fe278e53e80e296cec46a82
TheSkillCoder <100764233+TheSkillCoder@users.noreply.github.com>
2022-04-12 07:30:54 +0530

    Time: 50 ms (33.96%), Space: 13.9 MB (72.47%) - LeetHub

--- 50-powx-n/50-powx-n.py
+++ 50-powx-n/50-powx-n.py
@@ -1,22 +1,10 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if n == 0:
+        if not n:
             return 1
-        elif x in [0, 1] or n == 1:
-            return x 
+        elif n < 0:
+            return 1 / self.myPow(x, -n)
+        elif n % 2:
+            return x * self.myPow(x, n - 1)
         else:
-            pow = 1
-            if n < 0:
-                x = 1 / x
-                n = -(n + 1)
-                pow *= x
-
-            while n > 0:
-                if n % 2:
-                    pow *= x
-                    n -= 1
-                else:
-                    x *= x
-                    n //= 2
-            else:
-                return pow
+            return self.myPow(x * x, n // 2)
